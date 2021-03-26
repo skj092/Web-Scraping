8a4d49492996c6cb9f6053d6c0287ca6611f581d
skj092 <sonujha092@gmail.com>
2021-03-26 00:45:31 -0700

    formated

--- codes/9.py
+++ codes/9.py
@@ -1,10 +1,10 @@
 # Project: Downloading All XKCD Comics
 
-import os, requests, bs4 
+import os, requests, bs4
 
-url = 'https://xkcd.com/'
+url = "https://xkcd.com/"
 
-os.makedirs('../files/xkcd', exist_ok=True)    # store comics in ../files/xkcd
+os.makedirs("../files/xkcd", exist_ok=True)  # store comics in ../files/xkcd
 
 # Step 1: Design the Program
 
@@ -14,32 +14,32 @@ os.makedirs('../files/xkcd', exist_ok=True)    # store comics in ../files/xkcd
 # The first comic’s Prev button links to the https://xkcd.com/# URL, indicating that there are no more previous pages.
 
 # Step 2: Download the Web Page
-while not url.endswith('#'):
+while not url.endswith("#"):
     # Download the page.
-    print('Downloading page %s...' % url)
+    print("Downloading page %s..." % url)
     res = requests.get(url)
     res.raise_for_status()
 
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
 
     # Step 3: Find and Download the Comic Image.
-    comicElem = soup.select('#comic img')
+    comicElem = soup.select("#comic img")
     if comicElem == []:
-        print('Could not find comic image.')
+        print("Could not find comic image.")
     else:
-        comicUrl = 'https:' + comicElem[0].get('src')
+        comicUrl = "https:" + comicElem[0].get("src")
         # Download the image.
-        print('Downloading image %s...' % (comicUrl))
+        print("Downloading image %s..." % (comicUrl))
         res = requests.get(comicUrl)
         res.raise_for_status()
 
     # Step 4: Save the Image and Find the Previous Comic
-    imageFile = open(os.path.join('../files/xkcd', os.path.basename(comicUrl)),'wb')
+    imageFile = open(os.path.join("../files/xkcd", os.path.basename(comicUrl)), "wb")
     for chunk in res.iter_content(100000):
         imageFile.write(chunk)
     imageFile.close()
 
     # Get the Prev button's url.
     prevLink = soup.select('a[rel="prev"]')[0]
-    url = 'https://xkcd.com' + prevLink.get('href')
-print('Done.')
+    url = "https://xkcd.com" + prevLink.get("href")
+print("Done.")
